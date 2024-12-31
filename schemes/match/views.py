b730fef489c3fa59ed37368d8727ba5aa9ab56c2
Zattox <vladcsgoogurechik@mail.ru>
2024-12-31 13:32:49 +0300

    Create router for Match

---
+++ schemes/match/views.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+
+from schemes.match.schema import Match
+
+router = APIRouter(tags=['Match'])
+
+@router.get("/")
+async def get_player(
+    match: Match
+):
+    return {"match_id": match.id,
+            "first_team": match.first_team,
+            "second_team": match.second_team,
+            "tournament": match.tournament,
+            "date": match.date,
+            }
