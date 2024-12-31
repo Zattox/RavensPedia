9d641b5c6d20ffb90be94452147697ce1b550a8f
Zattox <vladcsgoogurechik@mail.ru>
2024-12-31 20:12:45 +0300

    Rename path

--- schemes/__init__.py
+++ schemes/__init__.py
@@ -5,7 +5,7 @@ from .team.views import router as team_router
 from .match.views import router as match_router
 from .tournament.views import router as tournament_router
 router = APIRouter()
-router.include_router(router=player_router, prefix='/player')
-router.include_router(router=team_router, prefix='/team')
-router.include_router(router=match_router, prefix='/match')
-router.include_router(router=tournament_router, prefix='/tournament')
+router.include_router(router=player_router, prefix='/players')
+router.include_router(router=team_router, prefix='/teams')
+router.include_router(router=match_router, prefix='/matches')
+router.include_router(router=tournament_router, prefix='/tournaments')
