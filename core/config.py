456efc1cff03c3c523ab035877a775be9fb1d749
Zattox <vladcsgoogurechik@mail.ru>
2025-01-02 01:07:13 +0300

    Create async-sqllite database

---
+++ core/config.py
@@ -0,0 +1,13 @@
+from os import getenv
+from pathlib import Path
+from pydantic_settings import BaseSettings
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+class Settings(BaseSettings):
+    db_url: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
+    db_echo: bool = False
+
+
+settings = Settings()
---
+++ core/models/__init__.py
@@ -0,0 +1,10 @@
+__all__ = (
+    "Base",
+    "DatabaseHelper",
+    "db_helper",
+    "Match",
+)
+
+from .base import Base
+from .db_helper import db_helper, DatabaseHelper
+from .match import Match
