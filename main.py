6898c60bf62a31bde5ce8d6b51fab42e14ebbb65
Zattox <vladcsgoogurechik@mail.ru>
2025-01-02 01:08:17 +0300

    Add lifespan for create database

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
-from schemes import router as router_v1
-app = FastAPI(
-    title="Ravens Pedia API",
-)
+from core.models import Base, db_helper
+from api_v1 import router as router_v1
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(title="Ravens Pedia API", lifespan=lifespan)
 app.include_router(router=router_v1)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True, port=8001)
-
