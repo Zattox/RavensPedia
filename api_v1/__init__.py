a65093f15bafd751c1a55c679959e69447bb5f30
Zattox <vladcsgoogurechik@mail.ru>
2025-01-02 01:11:31 +0300

    Rename files

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from .player.views import router as player_router
 from .team.views import router as team_router
-from .match.views import router as match_router
+from .match.crud import router as match_router
 from .tournament.views import router as tournament_router
 router = APIRouter()
 router.include_router(router=player_router, prefix='/players')
