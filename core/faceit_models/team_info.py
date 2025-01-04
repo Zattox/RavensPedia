be09f5d1ec599d23239a19061fc401551f643249
Zattox <vladcsgoogurechik@mail.ru>
2025-01-04 10:44:01 +0300

    Relocate class TeamStats in team_info.py

--- core/faceit_models/team_info.py
+++ core/faceit_models/team_info.py
@@ -1,12 +1,22 @@
 from sqlalchemy.orm import Mapped
 from sqlalchemy_utils import ScalarListType
 
-from .player_stats import PlayerStats
-from .team_stats import TeamStats
+from .player_info import PlayerInfo
+
+
+class TeamStats:
+    team: Mapped[str]
+    team_win: Mapped[int]
+    team_headshots: Mapped[float]
+
+    first_half_score: Mapped[int]
+    second_half_score: Mapped[int]
+    overtime_score: Mapped[int]
+    final_score: Mapped[int]
 
 
 class TeamInfo:
     faceit_team_id: Mapped[str]
     premade: Mapped[str]
     team_stats: TeamStats
-    players: ScalarListType[PlayerStats]
+    players: ScalarListType[PlayerInfo]
