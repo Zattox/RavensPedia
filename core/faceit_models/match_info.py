319ea3ab6e0c6a592742aa2ba635f18eef9c732c
Zattox <vladcsgoogurechik@mail.ru>
2025-01-04 18:23:27 +0300

    Change the separator for ScalarListType

--- core/faceit_models/match_info.py
+++ core/faceit_models/match_info.py
@@ -1,3 +1,4 @@
+from sqlalchemy.orm import mapped_column, Mapped
 from sqlalchemy_utils import ScalarListType
 
 from core.base import Base
@@ -7,4 +8,7 @@ from .round_info import RoundInfo
 class MatchInfo(Base):
     __tablename__ = "matches_info"
 
-    rounds: ScalarListType[RoundInfo]
+    faceit_match_id: Mapped[str]
+    rounds: Mapped[list[RoundInfo]] = mapped_column(
+        ScalarListType(RoundInfo, separator=";")
+    )
