13aa9079f8bdca77f149cb5839b940c90dc7874b
Zattox <vladcsgoogurechik@mail.ru>
2025-01-05 23:37:20 +0300

    Fix sql_table MatchInfo scheme

--- core/faceit_models/match_info.py
+++ core/faceit_models/match_info.py
@@ -1,5 +1,5 @@
+from sqlalchemy import PickleType
 from sqlalchemy.orm import mapped_column, Mapped
-from sqlalchemy_utils import ScalarListType
 
 from core.base import Base
 from .round_info import RoundInfo
@@ -9,6 +9,4 @@ class MatchInfo(Base):
     __tablename__ = "matches_info"
 
     faceit_match_id: Mapped[str]
-    rounds: Mapped[list[RoundInfo]] = mapped_column(
-        ScalarListType(RoundInfo, separator=";")
-    )
+    rounds: Mapped[list[RoundInfo]] = mapped_column(PickleType)
