5f96e70f40a8e4f4ad3c7caba2a81e4befdee7f2
Zattox <vladcsgoogurechik@mail.ru>
2025-01-05 23:38:25 +0300

    Add delete view + fix name funcs

--- api_v1/faceit_classes/match_info/crud.py
+++ api_v1/faceit_classes/match_info/crud.py
@@ -1,31 +1,42 @@
-import requests
+from fastapi import Depends
 from sqlalchemy import select
 from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.faceit_models import MatchInfo
-from .funcs import json_to_round_info
-from core.config import faceit_settings
 
 
 # A function for create a MatchInfo in the database
 async def create_match_info(
     session: AsyncSession,
-    faceit_match_id: str,
+    match_info_create: MatchInfo,
 ) -> MatchInfo:
-    headers = {
-        "Accept": "application/json",
-        "Authorization": f"Bearer {faceit_settings.faceit_api_key}",
-    }
-    response = requests.get(
-        f"{faceit_settings.faceit_base_url}/{faceit_match_id}/stats", headers=headers
-    )
-    data = json_to_round_info(response.json())
-    match_info_create: MatchInfo = MatchInfo(
-        faceit_match_id=faceit_match_id, rounds=data
-    )
     session.add(match_info_create)
     await session.commit()  # Make changes to the database
     # It is necessary if there are changes on the database side
-    # await session.refresh(match_info)
+    # await session.refresh(match_info_create)
     return match_info_create
+
+
+# A function for getting a Match by its id from the database
+async def get_match_info(
+    session: AsyncSession,
+    match_info_id: int,
+) -> MatchInfo | None:
+    return await session.get(MatchInfo, match_info_id)
+
+
+async def get_matches_info(session: AsyncSession) -> list[MatchInfo]:
+    statement = select(MatchInfo).order_by(MatchInfo.faceit_match_id)
+    result: Result = await session.execute(statement)
+    matches = result.scalars().all()
+    return list(matches)
+
+
+# A function for delete a Match from the database
+async def delete_match_info(
+    session: AsyncSession,
+    match_info: MatchInfo,
+) -> None:
+    await session.delete(match_info)
+    await session.commit()  # Make changes to the database
--- api_v1/faceit_classes/match_info/views.py
+++ api_v1/faceit_classes/match_info/views.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, status, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from . import crud
+from . import crud, dependencies
 from core.project_models import db_helper
-from .schemes import MatchInfo, MatchInfoCreate
+from .schemes import MatchInfo
 
 router = APIRouter(tags=["Matches Info"])
 
@@ -11,9 +11,35 @@ router = APIRouter(tags=["Matches Info"])
 # A view for create a match_info in the database
 @router.post("/", response_model=MatchInfo, status_code=status.HTTP_201_CREATED)
 async def create_match_info(
-    faceit_match_id: str,
+    match_info: MatchInfo = Depends(dependencies.get_data_from_faceit),
     session: AsyncSession = Depends(db_helper.session_dependency),
 ):
     return await crud.create_match_info(
-        session=session, faceit_match_id=faceit_match_id
+        session=session,
+        match_info_create=match_info,
     )
+
+
+# A view to get all the matches from the database
+@router.get("/", response_model=list[MatchInfo])
+async def get_matches_info(
+    session: AsyncSession = Depends(db_helper.session_dependency),
+):
+    return await crud.get_matches_info(session=session)
+
+
+# A view for getting a match by its id from the database
+@router.get("/{match_info_id}/", response_model=MatchInfo)
+async def get_match_info(
+    match_info: MatchInfo = Depends(dependencies.get_match_info_by_id),
+):
+    return match_info
+
+
+# A view for delete a match from the database
+@router.delete("/{match_info_id}/", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_match_info(
+    match_info: MatchInfo = Depends(dependencies.get_match_info_by_id),
+    session: AsyncSession = Depends(db_helper.session_dependency),
+) -> None:
+    await crud.delete_match_info(session=session, match_info=match_info)
