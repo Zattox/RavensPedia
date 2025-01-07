e0f917f910a7ecbbfbadca5b8a137961b4f516a7
Zattox <vladcsgoogurechik@mail.ru>
2025-01-07 16:52:59 +0300

    No more create all an app start

--- main.py
+++ main.py
@@ -3,15 +3,11 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
 from api_v1 import router as router_v1
-from core.project_models import Base, db_helper
 
 
 # Function for configuring the application (creating a database)
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # Application configuration
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
     yield
     # Clean up
 
