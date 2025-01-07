a3a704bebaacb255709ccead1e764db4349bc2e4
Zattox <vladcsgoogurechik@mail.ru>
2025-01-07 23:18:49 +0300

    Update tournament scheme

--- core/project_models/tournament.py
+++ core/project_models/tournament.py
@@ -1,21 +1,37 @@
-from core.base import Base
+from core import Base
+from core.associations_models import (
+    team_tournament_association_table,
+    player_tournament_association_table,
+)
+
 from typing import TYPE_CHECKING
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy_utils import ScalarListType
+
 
 if TYPE_CHECKING:
     from .match import Match
+    from .team import Team
+    from .player import Player
 
 
 class Tournament(Base):
     # The tournament name
     tournament_name: Mapped[str] = mapped_column(String(100), unique=True)
     # The prize of tournament
-    prize: Mapped[str] = mapped_column(String(50))
+    prize: Mapped[str | None] = mapped_column(String(50))
     # The description of the tournament
-    description: Mapped[str] = mapped_column(String(255))
+    description: Mapped[str | None] = mapped_column(String(255))
     # The IDs of the matches the tournament participated in
     matches: Mapped[list["Match"]] = relationship(back_populates="tournament")
+
     # The IDs of the teams the tournament participated in
-    teams: Mapped[list[int]] = mapped_column(ScalarListType(int))
+    teams: Mapped[list["Team"]] = relationship(
+        secondary=team_tournament_association_table,
+        back_populates="tournaments",
+    )
+
+    players: Mapped[list["Player"]] = relationship(
+        secondary=player_tournament_association_table,
+        back_populates="tournaments",
+    )
