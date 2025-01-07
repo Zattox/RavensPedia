81a6375a32ff2f8e3fe52884d34343a66a97e536
Zattox <vladcsgoogurechik@mail.ru>
2025-01-07 18:35:26 +0300

    Create relationships between match and tournament

--- core/project_models/match.py
+++ core/project_models/match.py
@@ -1,8 +1,11 @@
 from core.base import Base
-
+from typing import TYPE_CHECKING
 from datetime import datetime
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+if TYPE_CHECKING:
+    from .tournament import Tournament
 
 
 class Match(Base):
@@ -14,6 +17,9 @@ class Match(Base):
     # Additional information about the match
     description: Mapped[str] = mapped_column(String(255))
     # ID of the tournament in which the match is being played
-    tournament_id: Mapped[int]
+    tournament_id: Mapped[int] = mapped_column(
+        ForeignKey("tournaments.id"),
+    )
+    tournament: Mapped["Tournament"] = relationship(back_populates="matches")
     # Match start date
     date: Mapped[datetime]
--- core/project_models/tournament.py
+++ core/project_models/tournament.py
@@ -1,8 +1,11 @@
 from core.base import Base
-
+from typing import TYPE_CHECKING
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy_utils import ScalarListType
+
+if TYPE_CHECKING:
+    from .match import Match
 
 
 class Tournament(Base):
@@ -13,6 +16,6 @@ class Tournament(Base):
     # The description of the tournament
     description: Mapped[str] = mapped_column(String(255))
     # The IDs of the matches the tournament participated in
-    matches: Mapped[list[int]] = mapped_column(ScalarListType(int))
+    matches: Mapped[list["Match"]] = relationship(back_populates="tournament")
     # The IDs of the teams the tournament participated in
     teams: Mapped[list[int]] = mapped_column(ScalarListType(int))
