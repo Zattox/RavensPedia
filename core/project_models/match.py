ab5b6cceba7647625cfbab7766c8faff0daa3eaf
Zattox <vladcsgoogurechik@mail.ru>
2025-01-07 23:18:18 +0300

    Update match scheme

--- core/project_models/match.py
+++ core/project_models/match.py
@@ -1,26 +1,43 @@
-from core.base import Base
+from core import Base
+from core.associations_models import (
+    team_match_association_table,
+    player_match_association_table,
+)
+
 from typing import TYPE_CHECKING
 from datetime import datetime
-from sqlalchemy import String, ForeignKey
+from sqlalchemy import String, ForeignKey, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 if TYPE_CHECKING:
+    from .team import Team
     from .tournament import Tournament
+    from .player import Player
 
 
 class Match(Base):
     __tablename__ = "matches"
 
     # ID of the first participant of the match
-    first_team_id: Mapped[int]
-    # ID of the second participant of the match
-    second_team_id: Mapped[int]
+    teams: Mapped[list["Team"]] = relationship(
+        secondary=team_match_association_table,
+        back_populates="matches",
+    )
+
+    players: Mapped[list["Player"]] = relationship(
+        secondary=player_match_association_table,
+        back_populates="matches",
+    )
+
     # Additional information about the match
-    description: Mapped[str] = mapped_column(String(255))
+    description: Mapped[str | None] = mapped_column(String(255))
 
     # ID of the tournament in which the match is being played
     tournament_id: Mapped[int] = mapped_column(ForeignKey("tournaments.id"))
     tournament: Mapped["Tournament"] = relationship(back_populates="matches")
 
     # Match start date
-    date: Mapped[datetime]
+    date: Mapped[datetime] = mapped_column(
+        default=datetime.now,
+        server_default=func.now(),
+    )
