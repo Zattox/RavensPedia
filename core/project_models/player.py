e9e0e534562fda6cabcb3f536f93fc48744cdd69
Zattox <vladcsgoogurechik@mail.ru>
2025-01-07 23:18:26 +0300

    Update player scheme

--- core/project_models/player.py
+++ core/project_models/player.py
@@ -1,4 +1,8 @@
-from core.base import Base
+from core import Base
+from core.associations_models import (
+    player_tournament_association_table,
+    player_match_association_table,
+)
 
 from typing import TYPE_CHECKING
 from sqlalchemy import String, Integer, ForeignKey
@@ -7,6 +11,8 @@ from sqlalchemy_utils import ScalarListType
 
 if TYPE_CHECKING:
     from .team import Team
+    from .match import Match
+    from .tournament import Tournament
 
 
 class Player(Base):
@@ -16,15 +22,22 @@ class Player(Base):
         unique=True,
     )
     # The player's real name
-    name: Mapped[str] = mapped_column(String(15))
+    name: Mapped[str | None] = mapped_column(String(15))
     # The player's real surname
-    surname: Mapped[str] = mapped_column(String(30))
+    surname: Mapped[str | None] = mapped_column(String(30))
 
     # The ID of the player's current team
     team_id: Mapped[int] = mapped_column(ForeignKey("teams.id"))
     team: Mapped["Team"] = relationship(back_populates="players")
 
     # The IDs of the matches the player participated in
-    matches: Mapped[list[int]] = mapped_column(ScalarListType(int))
+    matches: Mapped[list["Match"]] = relationship(
+        secondary=player_match_association_table,
+        back_populates="players",
+    )
+
     # The IDs of the tournaments the team participated in
-    tournaments: Mapped[list[int]] = mapped_column(ScalarListType(int))
+    tournaments: Mapped[list["Tournament"]] = relationship(
+        secondary=player_tournament_association_table,
+        back_populates="players",
+    )
