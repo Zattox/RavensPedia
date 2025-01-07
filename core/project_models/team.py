d4c7286a7fecc0ac0794880d23050fd7766e9ebf
Zattox <vladcsgoogurechik@mail.ru>
2025-01-07 23:18:36 +0300

    Update team scheme

--- core/project_models/team.py
+++ core/project_models/team.py
@@ -1,23 +1,36 @@
-from core.base import Base
+from core import Base
+from core.associations_models import (
+    team_tournament_association_table,
+    team_match_association_table,
+)
+
 from typing import TYPE_CHECKING
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy_utils import ScalarListType
 
 if TYPE_CHECKING:
     from .player import Player
+    from .tournament import Tournament
+    from .match import Match
 
 
 class Team(Base):
     # The team name
     team_name: Mapped[str] = mapped_column(String(15), unique=True)
     # The description of the team
-    description: Mapped[str] = mapped_column(String(255))
+    description: Mapped[str | None] = mapped_column(String(255))
 
     # IDs of the main team members
     players: Mapped[list["Player"]] = relationship(back_populates="team")
 
     # The IDs of the matches the team participated in
-    matches: Mapped[list[int]] = mapped_column(ScalarListType(int))
+    matches: Mapped[list["Match"]] = relationship(
+        secondary=team_match_association_table,
+        back_populates="teams",
+    )
+
     # The IDs of the tournaments the team participated in
-    tournaments: Mapped[list[int]] = mapped_column(ScalarListType(int))
+    tournaments: Mapped[list["Tournament"]] = relationship(
+        secondary=team_tournament_association_table,
+        back_populates="teams",
+    )
