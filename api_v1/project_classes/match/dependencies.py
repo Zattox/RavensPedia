cb3b8d0c36ed2c7e24492456ba125f5214fc2e11
Zattox <vladcsgoogurechik@mail.ru>
2025-01-10 00:11:46 +0300

    Update func get_match_by_id

--- api_v1/project_classes/match/dependencies.py
+++ api_v1/project_classes/match/dependencies.py
@@ -1,22 +1,35 @@
 from typing import Annotated
 
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import Depends, HTTPException, status, Path
+from sqlalchemy.orm import selectinload
 
 from . import crud
 from core import db_helper
+from core import Match as TableMatch
 
 
 # A function for get a match from the database by id
 async def get_match_by_id(
     match_id: Annotated[int, Path],
     session: AsyncSession = Depends(db_helper.session_dependency),
-):
-    match = await crud.get_match(session=session, match_id=match_id)
+) -> TableMatch:
+    table_match = await session.scalar(
+        select(TableMatch)
+        .where(TableMatch.id == match_id)
+        .options(
+            selectinload(TableMatch.players),
+            selectinload(TableMatch.teams),
+            selectinload(TableMatch.tournament),
+        ),
+    )
+
     # If such an id does not exist, then throw an exception.
-    if match is None:
+    if table_match is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Match {match_id} not found",
+            detail=f"team {match_id} not found",
         )
-    return match
+
+    return table_match
