919b6d0eafb46081f124ccf1d2abc8e8697d6df2
Zattox <vladcsgoogurechik@mail.ru>
2025-01-10 00:10:31 +0300

    Add func get_team_by_name

--- api_v1/project_classes/team/dependencies.py
+++ api_v1/project_classes/team/dependencies.py
@@ -31,3 +31,28 @@ async def get_team_by_id(
             detail=f"team {team_id} not found",
         )
     return table_team
+
+
+# A function for get a Team from the database by id
+async def get_team_by_name(
+    team_name: str | None,
+    session: AsyncSession = Depends(db_helper.session_dependency),
+) -> TableTeam | None:
+    if team_name is None:
+        return None
+    table_team = await session.scalar(
+        select(TableTeam)
+        .where(TableTeam.team_name == team_name)
+        .options(
+            selectinload(TableTeam.players),
+            selectinload(TableTeam.matches),
+            selectinload(TableTeam.tournaments),
+        ),
+    )
+    # If such an id does not exist, then throw an exception.
+    if table_team is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"team {team_name} not found",
+        )
+    return table_team
