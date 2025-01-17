3df5a9972d5cd44538c63d4f545bf32d667dd3f8
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2025-01-17 21:11:20 +0300

    Delete match_info_router

--- ravenspedia/api_v1/__init__.py
+++ ravenspedia/api_v1/__init__.py
@@ -14,16 +14,11 @@ from ravenspedia.api_v1.project_classes.tournament.views import (
     manager_tournament_router,
 )
 
-# from ravenspedia.api_v1.faceit_classes.match_info.views import (
-#     router as match_info_router,
-# )
-
 router = APIRouter()
 router.include_router(router=player_router, prefix="/players")
 router.include_router(router=team_router, prefix="/teams")
 router.include_router(router=manager_team_router, prefix="/teams")
 router.include_router(router=match_router, prefix="/matches")
 router.include_router(router=manager_match_router, prefix="/matches")
-# router.include_router(router=match_info_router, prefix="/match_info")
 router.include_router(router=tournament_router, prefix="/tournaments")
 router.include_router(router=manager_tournament_router, prefix="/tournaments")
