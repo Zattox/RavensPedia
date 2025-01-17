7d1f48700db75c13b11aea093c0868f79385453b
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2025-01-17 21:21:59 +0300

    Delete match_info class

--- ravenspedia/api_v1/faceit_classes/match_info/funcs.py
+++
@@ -1,8 +0,0 @@
-from ravenspedia.core.faceit_models.round_info import RoundInfo
-
-
-def json_to_round_info(data: dict) -> list[RoundInfo]:
-    rounds = []
-    for round_data in data.get("rounds", []):
-        rounds.append(RoundInfo(**round_data))
-    return rounds
