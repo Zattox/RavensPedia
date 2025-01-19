96c1ff1f7fca0d6bf6d6f81cfbdd2294286500fd
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2025-01-19 04:29:48 +0300

    Add new exception

--- ravenspedia/api_v1/project_classes/team/team_management.py
+++ ravenspedia/api_v1/project_classes/team/team_management.py
@@ -1,10 +1,9 @@
 from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from ravenspedia.core import TableTeam, TablePlayer
 from .crud import table_to_response_form
 from .schemes import ResponseTeam
-
-from ravenspedia.core import TableTeam, TablePlayer
 
 
 async def add_player_in_team(
@@ -18,10 +17,16 @@ async def add_player_in_team(
             detail=f"Player {player.nickname} already exists",
         )
 
+    if player.team:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"The player {player.nickname} has already joined {player.team.name}",
+        )
+
     if len(team.players) == team.max_number_of_players:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f"The maximum number of teams will participate in the match",
+            detail=f"The maximum number of players will participate in team",
         )
 
     team.players.append(player)
