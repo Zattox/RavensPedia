590bf4bb9de26a20f4cbc665fa87a2933a0c41c3
Zattox <vladcsgoogurechik@mail.ru>
2025-01-25 17:49:22 +0300

    Add base func for jwt

---
+++ ravenspedia/api_v1/auth/utils.py
@@ -0,0 +1,90 @@
+from datetime import datetime, timedelta, timezone
+
+import jwt
+import uuid
+import bcrypt
+
+from ravenspedia.core.config import auth_settings
+
+
+def get_hash_password(
+    password: str,
+) -> bytes:
+    salt = bcrypt.gensalt()
+    pwd_bytes: bytes = password.encode("utf-8")
+    return bcrypt.hashpw(
+        password=pwd_bytes,
+        salt=salt,
+    )
+
+
+def validate_password(
+    password: str,
+    hashed_password: bytes,
+) -> bool:
+    pwd_bytes: bytes = password.encode("utf-8")
+    return bcrypt.checkpw(
+        password=pwd_bytes,
+        hashed_password=hashed_password,
+    )
+
+
+def encode_jwt(
+    payload: dict,
+    private_key: str = auth_settings.private_key_path.read_text(),
+    algorithm: str = auth_settings.algorithm,
+    expire_minutes: int = auth_settings.access_token_expire_minutes,
+    expire_timedelta: timedelta | None = None,
+) -> str:
+    to_encode = payload.copy()
+    now = datetime.now(timezone.utc)
+
+    if expire_timedelta:
+        expire = now + expire_timedelta
+    else:
+        expire = now + timedelta(minutes=expire_minutes)
+
+    to_encode.update(
+        iat=now,
+        exp=expire,
+        jti=str(uuid.uuid4()),
+    )
+
+    encoded_jwt = jwt.encode(
+        payload=to_encode,
+        key=private_key,
+        algorithm=algorithm,
+    )
+
+    return encoded_jwt
+
+
+def decode_jwt(
+    token: str | bytes,
+    public_key: str = auth_settings.public_key_path.read_text(),
+    algorithm: str = auth_settings.algorithm,
+):
+    decoded_jwt = jwt.decode(
+        jwt=token,
+        key=public_key,
+        algorithms=[algorithm],
+    )
+    return decoded_jwt
+
+
+def create_jwt(
+    token_type: str,
+    token_data: dict,
+    expire_minutes: int,
+    expire_timedelta: timedelta | None = None,
+) -> str:
+    jwt_payload = {
+        auth_settings.TOKEN_TYPE_FIELD: token_type,
+    }
+    jwt_payload.update(token_data)
+
+    return encode_jwt(
+        payload=jwt_payload,
+        expire_minutes=expire_minutes,
+        expire_timedelta=expire_timedelta,
+    )
