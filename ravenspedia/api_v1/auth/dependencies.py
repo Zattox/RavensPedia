f36d916ba17660ac8654e75dc809515a61c38b1e
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2025-02-02 16:52:13 +0300

    Add dependencies

---
+++ ravenspedia/api_v1/auth/dependencies.py
@@ -0,0 +1,73 @@
+from datetime import datetime, timezone
+
+from fastapi import Request, HTTPException, status, Depends
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from ravenspedia.core import TableUser, db_helper
+from . import utils
+
+
+def get_access_token(request: Request):
+    token = request.cookies.get("users_access_token")
+    if not token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token not found",
+        )
+    return token
+
+
+def get_refresh_token(request: Request):
+    token = request.cookies.get("users_refresh_token")
+    if not token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token not found",
+        )
+    return token
+
+
+async def get_current_user(
+    token: str = Depends(get_access_token),
+    session: AsyncSession = Depends(db_helper.session_dependency),
+) -> TableUser:
+    auth_exc = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid token",
+    )
+
+    try:
+        payload = utils.decode_jwt(token=token)
+        Request.state.device_id = payload["device_id"]
+    except Exception:
+        raise auth_exc
+
+    expire: str = payload.get("exp")
+    expire_time = datetime.fromtimestamp(int(expire), tz=timezone.utc)
+
+    if (expire is None) or (expire_time < datetime.now(timezone.utc)):
+        raise auth_exc
+
+    user_email = payload.get("sub")
+    if not user_email:
+        raise auth_exc
+
+    user: TableUser = await session.scalar(
+        select(TableUser).where(TableUser.email == user_email)
+    )
+    if not user:
+        raise auth_exc
+
+    return user
+
+
+async def get_current_admin_user(
+    current_user: TableUser = Depends(get_current_user),
+):
+    if current_user.is_admin:
+        return current_user
+    raise HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail="Insufficient permissions!",
+    )
