307007846f983945ac4211ca56430dc0ce8a0c2b
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2025-04-01 23:21:57 +0300

    Change in views player_id -> player_nickname

--- ravenspedia/api_v1/project_classes/player_stats/views.py
+++ ravenspedia/api_v1/project_classes/player_stats/views.py
@@ -6,13 +6,14 @@ from .crud import get_player_stats
 from .dependencies import get_stats_filter
 from .schemes import PlayerStatsFilter
 from ..player.crud import get_player_by_id
+from ..player.dependencies import get_player_by_nickname
 
 router = APIRouter(tags=["Players Stats"])
 
 
-@router.get("/{player_id}/")
+@router.get("/{player_nickname}/")
 async def get_player_stats(
-    player: TablePlayer = Depends(get_player_by_id),
+    player: TablePlayer = Depends(get_player_by_nickname),
     stats_filter: PlayerStatsFilter = Depends(get_stats_filter),
     session: AsyncSession = Depends(db_helper.session_dependency),
 ):
